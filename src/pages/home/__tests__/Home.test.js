ab016ec568db371b395fd90842793f7b554aee73
Evellyn Lima <evellynlrl@hotmail.com>
2019-11-30 19:34:20 -0300

    Adiciona mock da requisição

--- src/pages/home/__tests__/Home.test.js
+++ src/pages/home/__tests__/Home.test.js
@@ -1,18 +1,32 @@
 import React from 'react'
-import { render, wait } from '@testing-library/react'
+import { render, wait, cleanup } from '@testing-library/react'
 import '@testing-library/dom'
 
+import { fetchData } from 'api'
 import Home from '../'
+
+jest.mock('api')
+jest.setTimeout(6000)
+
+afterEach(cleanup)
 
 describe('Página home', () => {
   it('Deve renderizar 3 cards, Nuuk, Urubici e Nairobi', async () => {
     const { getByText } = render(<Home />)
 
     await wait(() => {
+      expect(fetchData).toBeCalledWith({ from: 'Nuuk,GL' })
       getByText('Nuuk, GL')
     })
 
-    getByText('Urubici, BR')
-    getByText('Nairobi, KE')
+    await wait(() => {
+      expect(fetchData).toBeCalledWith({ from: 'Urubici,BR' })
+      getByText('Urubici, BR')
+    })
+
+    await wait(() => {
+      expect(fetchData).toBeCalledWith({ from: 'Nairobi,KE' })
+      getByText('Nairobi, KE')
+    })
   })
 })
